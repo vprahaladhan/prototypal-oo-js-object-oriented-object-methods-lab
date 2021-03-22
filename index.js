17a89403570d00f2a5322ac33533bc2730e0fd37
prahal <vprahaladhan@gmail.com>
2021-03-22 21:50:31 +1100

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-class BoardMember {
-  constructor(name, homeState, training) {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
-  }
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+ this.training = training;
+}
 
-  veto() {
-    return 'No, I must disagree';
-  }
+BoardMember.prototype.veto = function() {
+  return 'No, I must disagree';
+}
 
-  approve() {
-    return 'You can do that!';
-  }
+BoardMember.prototype.approve = function() {
+  return 'You can do that!';
+}
 
-  doCharity() {
-    return 'I like to help people.';
-  }
+BoardMember.prototype.doCharity = function() {
+  return 'I like to help people.';
+}
 
-  releasePressStatement() {
-    return 'You will see great things from Scuber.';
-  }
+BoardMember.prototype.releasePressStatement = function () {
+  return 'You will see great things from Scuber.';
+}
 
-  sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-  }
+BoardMember.prototype.sayHi = function () {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 }
